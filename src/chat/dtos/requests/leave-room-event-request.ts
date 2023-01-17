8f9e1256db431ce5ef6758fd0f985bbfa535c997
Fernando Guerrero <devrrior@gmail.com>
2023-01-17 00:28:10 -0600

    format(chat): format

--- src/chat/dtos/requests/leave-room-event-request.ts
+++ src/chat/dtos/requests/leave-room-event-request.ts
@@ -1,3 +1,7 @@
-export default class LeaveRoomEventRequest {
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class LeaveRoomEventRequest {
+  @IsNotEmpty()
+  @IsString()
   roomId: string;
 }
