a1f9a09ea56a543dde5f5662bca139af97d1d733
Fernando Guerrero <devrrior@gmail.com>
2023-01-17 00:28:45 -0600

    fix(chat): add controller

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,10 +3,11 @@ import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatMessageEntity } from './chat-message.entity';
+import { ChatController } from './chat.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatMessageEntity])],
   providers: [ChatGateway, ChatService],
-  // controllers: [ChatController],
+  controllers: [ChatController],
 })
 export class ChatModule {}
